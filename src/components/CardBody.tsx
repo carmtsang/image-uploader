195acc12087e7729ad11d3b920bf15b5427b753a
carmtsang <carmen.tsang1208@hotmail.com>
2023-03-18 16:37:00 -0700

    add text

--- src/components/CardBody.tsx
+++ src/components/CardBody.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-const TEXT = "HELLO WORLD!";
+const UPLOAD_TITLE = "Upload your image";
+const UPLOAD_DESCRIPTION = "File should be Jpeg, Png...";
+// const OR = "or";
 
 const CardBody = () => {
   return (
-    <Card border="light" className="content-card">
-      <Card.Body>{TEXT}</Card.Body>
+    <Card border="light" className="content-card text-center">
+      <Card.Title>{UPLOAD_TITLE}</Card.Title>
+      <Card.Body>{UPLOAD_DESCRIPTION}</Card.Body>
     </Card>
   );
 };
