66a3a3a5f157f89701ebedb994ab81c210224d0b
carmtsang <carmen.tsang1208@hotmail.com>
2023-03-29 21:26:52 -0700

    add image placeholder & btn

--- src/components/CardBody.tsx
+++ src/components/CardBody.tsx
@@ -1,15 +1,25 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Button, Card, Image } from "react-bootstrap";
 
 const UPLOAD_TITLE = "Upload your image";
 const UPLOAD_DESCRIPTION = "File should be Jpeg, Png...";
 // const OR = "or";
 
 const CardBody = () => {
+  const handleSubmit = () => {};
+
   return (
     <Card border="light" className="content-card text-center">
       <Card.Title>{UPLOAD_TITLE}</Card.Title>
-      <Card.Body>{UPLOAD_DESCRIPTION}</Card.Body>
+      <Card.Body>
+        {UPLOAD_DESCRIPTION}
+        <div style={{ display: "block", width: 700, padding: 30 }}>
+          <Image src="/" />
+        </div>
+        <Button variant="primary" onClick={handleSubmit}>
+          Choose File
+        </Button>
+      </Card.Body>
     </Card>
   );
 };
