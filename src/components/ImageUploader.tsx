10a69326d632f18b834ecadfc2f2ca3bbe3a083a
carmtsang <carmen.tsang1208@hotmail.com>
2023-04-01 22:11:31 -0700

    add to dropzone/ remove style

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -1,78 +1,32 @@
-import { useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
-const baseStyle = {
-  flex: 1,
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  padding: "20px",
-  borderWidth: 2,
-  borderRadius: 2,
-  borderColor: "#eeeeee",
-  borderStyle: "dashed",
-  backgroundColor: "#fafafa",
-  color: "#bdbdbd",
-  outline: "none",
-  transition: "border .24s ease-in-out",
-};
-
-const focusedStyle = {
-  borderColor: "#2196f3",
-};
-
-const acceptStyle = {
-  borderColor: "#00e676",
-};
-
-const rejectStyle = {
-  borderColor: "#ff1744",
-};
-
 export default function ImageUploader() {
-    const {
-      getRootProps,
-      getInputProps,
-      isFocused
-      isDragAccept,
-      isDragReject,
-    } = useDropzone({ accept: { "image/*": [] } });
+  const [files, setFiles] = useState([]);
+  const { getRootProps, acceptedFiles, getInputProps } = useDropzone({
+    accept: { "image/*": [] },
+  });
 
-    const style = useMemo(
-      () => ({
-        ...baseStyle,
-        ...(isFocused ? focusedStyle : {}),
-        ...(isDragAccept ? acceptStyle : {}),
-        ...(isDragReject ? rejectStyle : {}),
-      }),
-      [isFocused, isDragAccept, isDragReject]
-    );
+  const onDrop = useCallback((acceptedFiles: []) => {
+    console.log(acceptedFiles);
+    // do something with the files
+  }, []);
 
-    return (
-      <div className="container">
-        <div {...getRootProps({ style: {} })}>
-          <input {...getInputProps()} />
-          <p>Drag 'n' drop some files here, or click to select files</p>
-        </div>
-      </div>
-    );
-  
-
-  // const files = acceptedFiles.map((file) => (
-  //   <li key={file.path}>
-  //     {file.path} - {file.size} bytes
-  //   </li>
-  // ));
+  const accepted = acceptedFiles.map((file: {}) => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
 
   return (
     <section className="container">
-      <div {...getRootProps({ className: "dropzone" })}>
+      <div {...getRootProps({ style: {} })}>
         <input {...getInputProps()} />
         <p>Drag 'n' drop some files here, or click to select files</p>
       </div>
       <aside>
         <h4>Files</h4>
-        {/* <ul>{files}</ul> */}
+        <ul>{accepted}</ul>
       </aside>
     </section>
   );
