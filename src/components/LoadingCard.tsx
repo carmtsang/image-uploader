ad1c98ac5a54ddc70948d21f87b6c08bf3f3a640
carmtsang <carmtsang89@hotmail.com>
2023-04-06 17:20:22 -0700

    use react component

--- src/components/LoadingCard.tsx
+++ src/components/LoadingCard.tsx
@@ -1,18 +1,14 @@
-import { Card } from 'react-bootstrap';
+import { Card, ProgressBar } from 'react-bootstrap';
 const LOADING = 'Loading...';
-export default function LoadingCard() {
+
+export interface LoadingProp {
+  now: number;
+}
+export default function LoadingCard({ now }: LoadingProp) {
   return (
     <Card border="light" className="content-card text-center">
       <Card.Title>{LOADING}</Card.Title>
-      <div className="progress">
-        {/* <div
-          className="progress-bar"
-          role="progressbar"
-          aria-valuenow="0"
-          aria-valuemin="0"
-          aria-valuemax="100"
-        ></div> */}
-      </div>
+      <ProgressBar now={now} />;
     </Card>
   );
 }
