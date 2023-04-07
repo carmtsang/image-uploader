6b57d6e95bf4611c6e6ffaf10743b3221d7d96b3
carmtsang <carmtsang89@hotmail.com>
2023-04-07 10:01:48 -0600

    start uploading func

--- src/components/upload/Uploader.tsx
+++ src/components/upload/Uploader.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { useCallback, useState } from 'react';
 import { Button, Image } from 'react-bootstrap';
 import { FileError, FileRejection, useDropzone } from 'react-dropzone';
-import image from '../../images/image.svg';
+import uploadImage from '../../images/image.svg';
 import SingleUploader from './SingleUploader';
 import { db, storage } from '../../firebase/firebaseSetup';
-import { addDoc } from 'firebase/firestore';
-import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { addDoc, collection, updateDoc } from 'firebase/firestore';
+import {
+  getDownloadURL,
+  ref,
+  uploadBytes,
+  uploadBytesResumable
+} from 'firebase/storage';
 
 const OR = 'or';
 
@@ -15,12 +20,35 @@ export interface UploadableFile {
   errors: FileError[];
 }
 
-export default function Uploader() {
+export interface UploaderProp {
+  setProgress: 
+}
+
+export default function Uploader({ setProgress }: UploaderProp) {
   const [files, setFiles] = useState<UploadableFile[]>([]);
+  const [file, setFile] = useState<File>();
   const [url, setURL] = useState(null);
 
   const handleUpload = async () => {
-    const fileRef = await addDoc(collection(db, 'images'), {});
+    const storageRef = ref(storage, `/files/${files.name}`);
+    const uploadFile = uploadBytesResumable(storageRef, file);
+    try {
+      uploadFile.on('state_changed', (snapshot) => {
+        const percentage = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+      });
+    } catch (e) {
+      console.log(e);
+    }
+    // await Promise.all(a
+    //   files.map((file) => {
+    //     const imageRef = ref(storage, `posts/${docRef.id}/${image.path}`)
+    //     uploadBytes(imageRef, image, "data_url").then( async () => {
+    //       await const downloadURL = await getDownloadURL(imageRef)
+    //     })
+    //   })
+    // )
   };
 
   // const handleFile = (e: React.SyntheticEvent) => {
@@ -42,7 +70,7 @@ export default function Uploader() {
     <>
       <div className="drop-area" {...getRootProps()}>
         <input {...getInputProps()} />
-        <Image id="upload-box-image" src={image} />
+        <Image id="upload-box-image" src={uploadImage} alt="drop area" />
         <p>Drag 'n' drop some files here</p>
       </div>
       <p>{OR}</p>
