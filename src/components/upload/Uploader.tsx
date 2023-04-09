b08a53af8d9699d328b221118e36f7d58546a1c7
carmtsang <carmtsang89@hotmail.com>
2023-04-08 18:30:12 -0600

    setFiles to setSelectedFiles

--- src/components/upload/Uploader.tsx
+++ src/components/upload/Uploader.tsx
@@ -16,45 +16,62 @@ import { UploaderProp } from '../UploadCard';
 
 const OR = 'or';
 
-export interface UploadableFile {
-  file: File;
-  errors: FileError[];
-}
-
 // export interface UploaderProp {
 //   setProgress?: Dispatch<SetStateAction<number>>;
 // }
 
+export interface UploadableFile {
+  file: File;
+}
+
 export default function Uploader({ setProgress }: UploaderProp) {
-  const [files, setFiles] = useState<UploadableFile[]>([]);
-  // const [file, setFile] = useState<File | undefined>(undefined);
+  const [selectedFiles, setSelectedFiles] = useState<UploadableFile[]>([]);
   const [url, setURL] = useState(null);
 
-  const handleUpload = async (e: React.SyntheticEvent) => {
-    if (!storage) return;
-    const storageRef = ref('images');
-    try {
-      await storageRef.child(files[0].file.name).put(files[0])
-     } catch (e) {
-      console.log('error', e);
-    }
-    }
-  };
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const accepted = acceptedFiles.map((file) => ({ file, errors: [] }));
 
-  // const handleFile = (e: React.SyntheticEvent) => {
-
-  // }
-
-  const onDrop = useCallback((accFiles: File[], rejFiles: FileRejection[]) => {
-    const accepted = accFiles.map((file) => ({ file, errors: [] }));
-    
-    setFiles((curr) => [...curr, ...accepted, ...rejFiles]);
+    setSelectedFiles((curr) => [...curr, ...accepted]);
   }, []);
 
-  const { getRootProps, open, getInputProps } = useDropzone({
+  const { getRootProps, open, getInputProps, acceptedFiles } = useDropzone({
     onDrop,
     multiple: true
   });
+
+  const files = acceptedFiles.map((file) => (
+    <li key={file.path}>{file.path}</li>
+  ));
+  // const handleUpload = async (e: React.SyntheticEvent) => {
+  //   if (!storage) return;
+  //   const storageRef = ref(storage, `/images/${file.name}`);
+  //   try {
+  //     await storageRef.child(files[0].file.name).put(files[0]);
+  //   } catch (e) {
+  //     console.log('error', e);
+  //   }
+  // };
+  const handleUpload = (files: File[]) => {
+    files.map((file) =>
+      Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      })
+    );
+    setSelectedFiles(files);
+  };
+
+  const resetImage = () => {
+    setSelectedFiles([]);
+  };
+
+  // const handleFile = (e: React.SyntheticEvent) => {
+  // }
+
+  // const onDrop = useCallback((accFiles: File[], rejFiles: FileRejection[]) => {
+  //   const accepted = accFiles.map((file) => ({ file, errors: [] }));
+  //   setFiles((curr) => [...curr, ...accepted, ...rejFiles]);
+  //   setFile(accFiles[0]);
+  // }, []);
 
   return (
     <>
@@ -66,9 +83,10 @@ export default function Uploader({ setProgress }: UploaderProp) {
       <p>{OR}</p>
       <Button onClick={open}>Choose a File</Button>
 
-      {files.map((fileWrapper) => (
+      <ul>{files}</ul>
+      {/* {files.map((fileWrapper) => (
         <p key={fileWrapper.file.name}>{fileWrapper.file.name}</p>
-      ))}
+      ))} */}
     </>
   );
 }
