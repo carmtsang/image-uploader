387058a28c726f5820aa868094380fa0dffbca3a
carmtsang <carmtsang89@hotmail.com>
2023-04-21 16:10:40 -0700

    combine UploadCard and Uploader

--- src/components/UploadCard.tsx
+++ src/components/UploadCard.tsx
@@ -1,36 +1,99 @@
-import React, { Dispatch, SetStateAction } from 'react';
-import { Card } from 'react-bootstrap';
+import React, { Dispatch, SetStateAction, useCallback } from 'react';
+import { Card, Image, Button } from 'react-bootstrap';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { storage } from '../firebase/firebaseSetup';
+import { useDropzone } from 'react-dropzone';
 
-import Uploader from './upload/Uploader';
+import UploadImage from '../images/image.svg';
 
 const UPLOAD_TITLE = 'Upload your image';
 const UPLOAD_DESCRIPTION = 'File should be Jpeg, Png...';
+const OR = 'or';
 
 export interface UploaderProp {
   setProgress: Dispatch<SetStateAction<number>>;
   setCardBody: Dispatch<SetStateAction<number>>;
   setUrls: Dispatch<SetStateAction<string[]>>;
+  setFiles: Dispatch<SetStateAction<File[]>>;
 }
 
 export default function UploadCard({
   setProgress,
   setCardBody,
-  setUrls
+  setUrls,
+  setFiles
 }: UploaderProp) {
+  // const handleUpload = async (files: File[]) => {
+  //   try {
+  //     if (!files.length || files === null) return;
+  //     await Promise.all(
+  //       files.map(async (file) => {
+  //         const imageRef = ref(
+  //           storage,
+  //           `images/${file.lastModified}${file.name}`
+  //         );
+  //         await uploadBytesResumable(imageRef, file);
+  //         const url = await getDownloadURL(imageRef);
+  //         setUrls((prev) => [...prev, url]);
+  //         console.log('uploaded');
+  //       })
+  //     );
+  //   } catch (error) {
+  //     console.error(error);
+  //   } finally {
+  //     handleCardChange(2);
+  //   }
+  // };
+
+  const handleUpload = (files: File[]) => {
+    if (!files.length || files === null) return;
+    setCardBody(1);
+    return files.forEach((file) => {
+      const imageRef = ref(storage, `images/${file.lastModified}${file.name}`);
+      const uploadTask = uploadBytesResumable(imageRef, file);
+
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setProgress(progress);
+        },
+        (error) => console.log(error),
+        () =>
+          getDownloadURL(imageRef).then((url) =>
+            setUrls((prev) => [...prev, url])
+          )
+      );
+    });
+  };
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Do something with the files
+    acceptedFiles.map((file) => setFiles((prev) => [...prev, file]));
+  }, []);
+
+  const { getRootProps, open, getInputProps } = useDropzone({
+    onDrop,
+    multiple: true
+  });
+
   return (
     <>
       <Card.Title>{UPLOAD_TITLE}</Card.Title>
       <Card.Body id="upload-section">
         {UPLOAD_DESCRIPTION}
         <div className="image-uploader">
-          <Uploader
-            setProgress={setProgress}
-            setCardBody={setCardBody}
-            setUrls={setUrls}
-          />
+          <div className="drop-area" {...getRootProps()}>
+            <input {...getInputProps()} />
+            <Image id="upload-box-image" src={UploadImage} alt="drop area" />
+            <p>Drag 'n' drop some files here</p>
+          </div>
+          <p>{OR}</p>
+          <Button onClick={open}>Choose a File</Button>
         </div>
       </Card.Body>
-      <Card.Body id="preview-secton"></Card.Body>
     </>
   );
 }
